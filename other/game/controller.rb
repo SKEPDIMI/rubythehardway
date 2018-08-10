04d818aee76c7ddd930224c696f352a0bfb88289
John S <diaz.john312@gmail.com>
2018-08-10 15:39:15 -0400

    Refactor of addTime
    This function has become quite sophisticated. addTime will turn real life seconds into ingame time / days. 1 game second = 1.7 * real second

--- other/game/controller.rb
+++ other/game/controller.rb
@@ -10,24 +10,33 @@ module Controller
   def set(prop, value)
     @data[prop] = value
   end
-  def addTime(seconds = 0) # Changes from real life seconds to game time and adds to it
-    @data['time'] += (seconds * 0.05)
-    if (@data['time'] > 24)
-      @data['time'] = 0
-    end
+  def addTime(seconds = 0) # Changes from real life seconds to game hours / days
+    # 8.64 seconds in a day
+    # 1 second in game time is 0.0001
+    # 1 real second is 70% times faster in game
+
+    seconds_in_day = 86400
+    game_time_factor = 0.00017 #0.0001 (1 game sec) * 1.7 (70%)
+    seconds_in_game_day = seconds_in_day * game_time_factor
+
+    game_seconds = (seconds * game_time_factor) # Game seconds are %70 faster
+    days_passed = (game_seconds * seconds_in_game_day).floor
+
+    @data['time'] += game_seconds % seconds_in_game_day
+    @data['days'] = (@data['time'] / 24).floor
   end
-  def getTimeOfDay
+  def timeOfDay
     time = @data['time']
     case
-    when time <= 1
+    when time <= 0 && time <= 5 # 12AM - 5AM
       return 'midnight'
-    when time >= 11 && time <= 13
-      return 'midday'
-    when time >= 1 && time <= 11
+    when time >= 5 && time <= 11 # 5AM - 11AM
       return 'morning'
-    when time >= 13 && time <= 18
+    when time >= 11 && time <= 13 # 11AM - 1PM
+      return 'midday'
+    when time >= 13 && time <= 18 # 1PM - 6PM
       return 'afternoon'
-    when time >= 18 && time <= 0
+    when time >= 18 && time <= 0 # 6PM - 12AM
       return 'evening'
     end
   end
